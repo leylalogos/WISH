8155ea851b8e50d87d513a140191b8975a02a330
Abilogos <dev.abi.log@gmail.com>
2023-09-03 18:53:02 +0300

    fixing vroken routes & links

--- resources/views/layouts/inc/navbar.blade.php
+++ resources/views/layouts/inc/navbar.blade.php
@@ -1,16 +1,16 @@
 <nav class="navbar navbar-expand-lg navbar-dark" style="background:deeppink;">
-  
+
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav ml-auto">
       <li class="nav-item text-right">
-        <a class="nav-link text-right" href="{{route('home')}}">برو به صفحه اصلی</a>
+        <a class="nav-link text-right" href="{{route('index')}}">برو به صفحه اصلی</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="{{url('about')}}"> هدف سایت ما چیه</a>
+        <a class="nav-link" href="{{route('about')}}"> هدف سایت ما چیه</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">با ما ارتباط بگیر</a>
+        <a class="nav-link" href="{{route('contact')}}">با ما ارتباط بگیر</a>
       </li>
     </ul>
   </div>
