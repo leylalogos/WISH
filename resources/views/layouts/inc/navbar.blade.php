8abe596dc2ed85d9231f81bcfc52e7290732c9ea
Abilogos <dev.abi.log@gmail.com>
2023-09-04 11:29:42 +0300

    Change the styles and the logo has been added.

--- resources/views/layouts/inc/navbar.blade.php
+++ resources/views/layouts/inc/navbar.blade.php
@@ -1,16 +1,25 @@
-<nav class="navbar navbar-expand-lg navbar-dark" style="background:deeppink;">
-
-
+<nav class="navbar navbar-expand-lg navbar-dark">
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav ml-auto">
+    <li class="nav-item text-right">
+        <a class="nav-link text-right" href="{{route('index')}}">
+            <img id="navbar-logo" src="{{url('frontend/images/Wish.png')}}"/>
+        </a>
+      </li>
       <li class="nav-item text-right">
-        <a class="nav-link text-right" href="{{route('index')}}">برو به صفحه اصلی</a>
+        <a class="nav-link text-right" href="{{route('index')}}">
+            {{__('navbar.index')}}
+        </a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="{{route('about')}}"> هدف سایت ما چیه</a>
+        <a class="nav-link" href="{{route('about')}}">
+            {{__('navbar.about')}}
+        </a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="{{route('contact')}}">با ما ارتباط بگیر</a>
+        <a class="nav-link" href="{{route('contact')}}">
+            {{__('navbar.contact')}}
+        </a>
       </li>
     </ul>
   </div>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,6 +2,32 @@
 
 @section('content')
 
-<h1>This is home page</h1>
+<div class="index-card"  style="margin-top:0px;">
+        <div class="text-center mt-5">
+            <h1> {{__('static.slogan')}} </h1>
+            <button class="btn btn-success mt-5">
+                {{__('static.start creating my wish list')}}
+                &#X1F58A;&#XFE0F;
+            </button>
+        </div>
+    </div>
+<div class="container">
 
+    <div class="index-card">
+        <div class="text-center mt-5">
+            <h1> {{__('static.goal')}} </h1>
+        </div>
+        <div class="text-center static-text">
+            {!!nl2br(__('texts.about'))!!}
+        </div>
+    </div>
+    <div class="index-card">
+    <div class="text-center mt-5">
+            {{__('static.invite to login')}}
+        </div>
+        <div class="text-center mt-5">
+            Login/Sigup with Google
+        </div>
+    </div>
+</div>
 @endsection
