da2bc3b98a4283ade6aa7cd34a45dfc48f5901b7
ǃшɒʞɒH ǃǀɄ <dev.abi.log@gmail.com>
2023-11-22 10:16:20 +0330

    Update deploy.sh
    add Maintenance activation code while in Maintenance of the application

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #this code will be run in the ./.git/hooks/post-receive
+php artisan down --refresh=15 --message='Web Application is under Maintenance ...'
 composer install --optimize-autoloader --no-dev
 php artisan migrate --force -vv
 npm install
@@ -7,4 +8,4 @@ npm run build
 php artisan config:cache
 php artisan view:cache
 php artisan route:cache
-
+php artisan up
