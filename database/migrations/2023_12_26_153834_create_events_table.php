4f5a4709a9b3d0273b607435ca6e27512cb4365b
Leyla Sabouri <dev.leyla.sabouri@gmail.com>
2023-12-27 20:23:05 +0300

    make model and migration and controller for Event and use soft delete

---
+++ database/migrations/2023_12_26_153834_create_events_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->date('date');
+            $table->unsignedInteger('importance');
+            $table->string('description')->nullable();
+            $table->softDeletes();
+            $table->integer();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
